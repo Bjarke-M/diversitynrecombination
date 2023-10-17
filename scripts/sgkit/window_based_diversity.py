970d0b8997d66104fff971a6b1f08853837a078f
Bjarke-M <bjarke.meyer.pedersen@gmail.com>
2023-10-17 14:35:32 +0200

    changes

--- scripts/sgkit/window_based_diversity.py
+++ scripts/sgkit/window_based_diversity.py
@@ -1,21 +1,11 @@
 import sgkit as sg
 import numpy as np
-
-
-# Load genotype data
-genotypes = sg.load_dataset("1000G_phase3_chr22")
-
-# Define window size and step size
-window_size = 100000
-step_size = 50000
-
-# Calculate window-based heterozygosity
-heterozygosity = sg.stats.window_heterozygosity(
-    genotypes=genotypes,
-    size=window_size,
-    step=step_size,
-    backend="numpy"
-)
-
-# Print the results
-print(heterozygosity)
+from sgkit.io.vcf import vcf_to_zarr
+#import bcfs
+#vcf_to_zarr("/home/bjarkemp/primatediversity/people/bjarkemp/diversitynrecombination/data/liftover/Daubentonia_madagascariensis/bcfs/Daubentonia_madagascariensis_lifted.bcf.gz", "output.zarr")
+#vcf_to_zarr("/home/bjarkemp/primatediversity/people/bjarkemp/diversitynrecombination/data/liftover/Gorilla_gorilla_gorilla/bcfs/Gorilla_gorilla_gorilla_lifted.bcf.gz", "output_gorilla.zarr")
+ds = sg.load_dataset("output_gorilla.zarr")
+# make windows in the data frame:
+print(ds)
+#test = sg.individual_heterozygosity(ds)['call_heterozygosity']
+#print(test)
